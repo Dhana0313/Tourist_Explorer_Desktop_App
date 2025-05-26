32a7f118b8345e9ebbd83b9fe8437f27084a4a2a
Kalpani1205 <kalpanigunathilaka11@gmail.com>
2025-05-26 21:08:15 +0530

    completed welcome part

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,8 @@
-module com.tourist_explorer {
+module com.example.welcome2 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
 
-    opens com.tourist_explorer to javafx.fxml;
-    opens com.tourist_explorer.controllers to javafx.fxml; // 🔥 Required for your controller
 
-    exports com.tourist_explorer;
-    exports com.tourist_explorer.controllers to javafx.fxml; // Optional but good for clarity
+    opens com.example.welcome2 to javafx.fxml;
+    exports com.example.welcome2;
 }
